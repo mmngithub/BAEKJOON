d5bad2f40303140e4ddfa5328d20cb270b7bf2e0
mmngithub <157390321+mmngithub@users.noreply.github.com>
2024-02-12 21:52:43 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	if (90 <= x && x <= 100)
+		printf("A");
+	else if (80 <= x && x <= 89)
+		printf("B");
+	else if (70 <= x && x <= 79)
+		printf("C");
+	else if (60 <= x && x <= 69)
+		printf("D");
+	else 
+		printf("F");
+}
