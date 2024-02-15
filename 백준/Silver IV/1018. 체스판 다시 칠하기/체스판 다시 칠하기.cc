2447217f472f5f737e45fec5acec9f1a801830a1
mmngithub <157390321+mmngithub@users.noreply.github.com>
2024-02-15 22:44:00 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver IV/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,73 @@
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string WB[8] =
+{
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW"
+};
+string BW[8] =
+{
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB"
+};
+string w[50];
+int WB_cnt(int x, int y){
+	int cnt = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (WB[i][j]!=w[i+x][j+y]) {
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+int BW_cnt(int x, int y) {
+	int cnt = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (BW[i][j] != w[x+i][y+j]) {
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+
+
+
+
+
+int main() {
+	pair <int, int> p;
+	cin >> p.first >> p.second;
+	int result=64;
+	int tmp;
+	for (int i = 0; i < p.first; i++) {
+		cin >> w[i];
+	}
+	for (int i = 0; i+8 <= p.first; i++) {
+		for (int j = 0; j+8 <= p.second; j++) {
+			tmp = min(WB_cnt(i, j), BW_cnt(i, j));
+			if (tmp < result) {
+				result = tmp;
+			}
+		}
+	}
+	cout << result;
+}
