01e5ed090096a9318cb67a8c2d4db8fb0a0e81ce
mmngithub <157390321+mmngithub@users.noreply.github.com>
2024-02-16 06:50:10 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver III/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver III/1929. 소수 구하기/소수 구하기.cc
@@ -1,11 +1,9 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
 int arr[1000000] = { 0 };
 int main() {
 	int M, N;
 	cin >> M >> N;
-	
 	for (int i = 2; i <= N; i++) {
 		arr[i] = i;
 	}
@@ -13,13 +11,13 @@ int main() {
 		if (arr[i] == 0) {
 			continue;
 		}
-		for (int j = 2*i; j <= N; j += i) {
+		for (int j = i*i; j <= N; j += i) {
 			arr[j] = 0;
 		}	
 	}
 	for (int i = M; i <= N; i++) {
 		if (arr[i] != 0) {
-			printf("%d\n", arr[i]);
+			cout << arr[i] << "\n";
 		}
 	}
 }
