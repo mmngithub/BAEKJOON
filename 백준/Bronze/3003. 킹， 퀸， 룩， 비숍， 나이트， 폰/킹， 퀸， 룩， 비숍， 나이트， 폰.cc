283359b44954044e67a58c80c2e1a6a2bd5d455f
mmngithub <157390321+mmngithub@users.noreply.github.com>
2024-02-17 23:36:39 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a = 1, b = 1, c = 2, d = 2, e = 2, f = 8;
+	int q, w, i, r, t,u;
+	scanf("%d %d %d %d %d %d", &q, &w, &i, &r, &t, &u);
+	cout << a-q << " " << b-w << " " << c-i << " " << d-r << " " << e-t << " " << f-u;
+
+}
