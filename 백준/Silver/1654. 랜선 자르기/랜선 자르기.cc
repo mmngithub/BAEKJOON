132f9338df8011f6325ea2909993c7472bc079e4
mmngithub <157390321+mmngithub@users.noreply.github.com>
2024-02-20 22:30:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 12 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+using namespace std;
+long long v[10001];
+int main() {
+	int k, n;
+	cin >> k >> n;
+	for (int i = 0; i < k; i++) {
+		cin >> v[i];
+	}
+	int result = 0;
+	long long start = 1;
+	long long end = *max_element(v, v + k);
+	while (start <= end) {
+		int count = 0;
+		long long mid = (start + end) / 2;
+		for (int i = 0; i < k; i++) {
+			count += v[i]/mid;
+		}
+		if (count >= n) {
+			start = mid + 1;			
+				result = mid;			
+		}
+		else {
+			end = mid -1;
+		}
+	}
+	cout << result;
+}
